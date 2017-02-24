da1fe513a315c5cd398f29adf1c546bf936bd5e4
Jason Butwell <jasonbutwell@gmail.com>
2017-02-24 16:37:57 +0000

    Added Preference change listener to listen for changes to the settings preference fragment. The value of the preference is shown in UI.

--- app/src/main/java/com/jasonbutwell/settingspreferencefragmentexample/MainActivity.java
+++ app/src/main/java/com/jasonbutwell/settingspreferencefragmentexample/MainActivity.java
@@ -7,8 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +17,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+
+        // Shows what the sort by preference actually is in the UI
+        setSortByView( getSortByPreference(sharedPreferences) );
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        PreferenceManager.getDefaultSharedPreferences(this)
+                .unregisterOnSharedPreferenceChangeListener(this);
     }
 
     @Override
@@ -30,13 +43,38 @@ public class MainActivity extends AppCompatActivity {
 
         // If action was the settings
         if ( id == R.id.action_settings ){
-
-            Intent startSettingsActivity = new Intent(this, SettingsActivity.class);
-            startActivity(startSettingsActivity);
-
+            startActivity(new Intent(this, SettingsActivity.class));
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    // Sets a sort by UI view
+
+    public void setSortByView( String stringValue ) {
+        TextView tv = (TextView)findViewById(R.id.tv_sortby_info);
+        tv.setText(stringValue);
+    }
+
+    // gets our sort by preference value
+
+    public String getSortByPreference( SharedPreferences sharedPreferences ) {
+        return getPreferenceValue( sharedPreferences, getResources().getString(R.string.pref_sortby_key) ,R.string.pref_sortby_option_value_latest );
+    }
+
+    // gets a preference value based on its key
+
+    public String getPreferenceValue( SharedPreferences sharedPreferences, String key, int defaultValue ) {
+        return sharedPreferences.getString(key,getResources().getString(defaultValue));
+    }
+
+    // Called every time a preference is changed
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if ( key.equals(getString(R.string.pref_sortby_key)) ) {
+            String value = getPreferenceValue( sharedPreferences, key, R.string.pref_sortby_option_value_latest);
+            setSortByView(value);
+        }
     }
 }
